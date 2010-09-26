c77da3a5bc25157496818e94383ed5ecba819968
robtwiley <robtwiley@cox.net>
2010-09-26 14:58:37 -0500

    Initial setup

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab02Auth::Application.config.secret_token = '5ef79a8e402bde851c7c2bf7095fe7178d56793439f9737980ff7c4f8680f4274c2b85a771246a2040073b21db00a02f752a291d2793237b1750026d84f8a012'
